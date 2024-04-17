6134f364ae1d357c630cddc5a4534b60de63d043
Josue Lubaki <josuelubaki@gmail.com>
2024-04-17 15:40:33 -0400

    Refactor Error Messages

--- GHFollowers/Managers/NetworkManager.swift
+++ GHFollowers/Managers/NetworkManager.swift
@@ -18,29 +18,29 @@ class NetworkManager {
     func getFollowers(
         for username : String,
         page : Int,
-        completed : @escaping([Follower]?, String?) -> Void
+        completed : @escaping([Follower]?, ErrorMessage?) -> Void
     ) {
         let endpoint = baseURL + "/users/\(username)/followers?per_page=100&page=\(page)"
         
         guard let url = URL(string: endpoint) else {
-            completed(nil, "This username created an invalid request. Please try again.")
+            completed(nil, .INVALID_USERNAME)
             return
         }
         
         let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
             
             if let _ = error {
-                completed(nil, "Unable to complete your request. Please check your internet connection")
+                completed(nil, .UNABLE_TO_COMPLETE)
                 return
             }
             
             guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
-                completed(nil, "Invalid response from the server. Please try again.")
+                completed(nil, .INVALID_RESPONSE)
                 return
             }
             
             guard let data = data else {
-                completed(nil, "The data received on the server was invalid. Please try again")
+                completed(nil, .INVALID_DATA)
                 return
             }
             
@@ -51,7 +51,7 @@ class NetworkManager {
                 let followers = try decoder.decode([Follower].self, from: data)
                 completed(followers, nil)
             } catch {
-                completed(nil, "The data received on the server was invalid. Please try again")
+                completed(nil, .INVALID_DATA)
             }
         }
         
